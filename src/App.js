e77060d4d4aea76b7409c35955b71ea4dd3d38ca
sumitshinde2608 <shindesumit268@gmail.com>
2021-05-01 01:35:53 +0530

    Login Process

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Homepage from './Components/Homepage/Homepage'
+import Login from'./Components/Login/Login'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return(
     <div className = 'App'>
-      <Homepage/>
+      {/* <Homepage/> */}
+      <Login/>
     </div>
   )
 }
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+// import Form from 'react-bootstrap/Form'
+
+import Button from 'react-bootstrap/Button'
+import './Login.css'
+
+ const Login = () => {
+    const [Name,setName] = useState(' ');
+     return(
+       <div className="main"> 
+       <div className="Outer">
+      <div className='Logo'>
+      <h1>PLEX</h1>
+      </div>
+      <div className="LoginModule">
+        <div><input placeholder = '  Name' className = 'JoinInput upper'  type="text" onChange={(event) => setName(event.target.value)}/></div>
+        <div><input placeholder = '  Password' className = 'JoinInput' type="text" onChange={(event) => setName(event.target.value)}/></div>
+        <Button variant="outline-danger" >Login</Button>
+        <p>New here? <a href = "#">Sign up</a></p>
+      </div>
+      </div>
+      </div>
+     )
+ }
+
+ export default Login;
