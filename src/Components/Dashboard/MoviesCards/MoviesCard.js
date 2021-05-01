eb985bf04897df87ae81f3666f623a5f35947887
sumitshinde2608 <shindesumit268@gmail.com>
2021-05-01 17:17:55 +0530

    Added Share Button Fuctionalities

--- src/Components/Dashboard/MoviesCards/MoviesCard.js
+++ src/Components/Dashboard/MoviesCards/MoviesCard.js
@@ -5,10 +5,20 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import ListGroupItem from 'react-bootstrap/ListGroupItem'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './moviescard.css';
+import {
+  FacebookMessengerShareButton,
+  WhatsappShareButton,
+  InstapaperShareButton,
+  TelegramShareButton
+} from "react-share"
 
- const Cards = (props) => {
-     const { name, Bio, year, id} = props
-     const age = 2021-year;
+import {FaFacebookMessenger} from 'react-icons/fa'
+import {FaWhatsapp} from 'react-icons/fa'
+import {FaTelegramPlane} from 'react-icons/fa'
+// import ShareBtn from 'react-share-button';
+
+const Cards = (props) => {
+     const { name, Bio, id} = props
     
      return(
     <div>
@@ -16,13 +26,19 @@ import './moviescard.css';
     style={{ margin : '50px', justify:'center', color:'#222'}} className='card'>
   <Card.Img variant="top" src="https://picsum.photos/100" />
   <Card.Body>
-    <Card.Title>{name}</Card.Title>
-    <Card.Text>
+    <Card.Title className="TitleName">{name}</Card.Title>
+    <Card.Text className="Text">
       {Bio}
     </Card.Text>
   </Card.Body>
   <ListGroup className="list-group-flush">
     <ListGroupItem><a href="#">Watch</a></ListGroupItem>
+    <ListGroupItem><FacebookMessengerShareButton appId={301995351439710}url={'/Dashboard'}>Messenger  <FaFacebookMessenger/></FacebookMessengerShareButton></ListGroupItem>
+    <ListGroupItem><WhatsappShareButton title={name} url={'/Dashboard'}>WhatsApp  <FaWhatsapp/></WhatsappShareButton></ListGroupItem>
+    <ListGroupItem><TelegramShareButton title={name} url={'/Dashboard'}>Telegram  <FaTelegramPlane/></TelegramShareButton></ListGroupItem>
+
+
+
   </ListGroup>
 </Card>
 </div>
